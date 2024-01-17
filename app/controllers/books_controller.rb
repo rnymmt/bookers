c404773d54428c0b83e7fd93bd774d64e1ad9f56
EC2 Default User <ec2-user@ip-172-31-28-28.ap-northeast-1.compute.internal>
2024-01-17 12:02:56 +0000

    あと編集画面のバリデーション設定

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,20 +1,19 @@
 class BooksController < ApplicationController
-  def new
-    @books =Book.new
-  end
   
   def create
-    book = Book.new(book_params)
-    if book.save
-      flash[:notice] = "Book was successfully destroyed."
-      redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
     else
-      render :new
+      @books = Book.all
+      render :index
     end
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -27,13 +26,18 @@ class BooksController < ApplicationController
   
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    if book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(book.id)
+    else
+      render :edit
+    end
   end
   
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = "Book was successfully destroyed."
     redirect_to '/books'
   end
   
